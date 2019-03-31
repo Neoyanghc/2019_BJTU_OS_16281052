a2e76b6d7f70aa1757f94b7f108a3368e461d6ec
jackson <1178894864@qq.com>
2019-03-31 15:44:57 +0800

    lab3

--- lab2/fork_4_1.c
+++ lab2/fork_4_1.c
@@ -1,50 +1,34 @@
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 
-#include <unistd.h>
+#include "pch.h"
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
-int main(void)
+int main()
 {
-    int i = 0;
-    pid_t fpid,ppid,ppid2;
-    //pid指当前进程的pid,
-    //fpid指fork返回给当前进程的值
-    ppid = getppid();
-    ppid2 = getpid();
-    printf("the first node of tree‘s father %d\n",ppid);
-    for (i = 0; i < 2; i++)
-    {
-        fpid = fork();
-        if (fpid == 0)
-        {
-            // printf("%d child  %4d %4d %4d\n", i, getppid(), getpid(), fpid);
-        }
-        else{
-            printf("%d parent %4d %4d\n", i, getpid(), fpid);
-        }
-        if (i == 1 && fpid != 0 && ppid != getppid())
-        {
-            fpid = fork();
-            if (fpid == 0)
-            {
-                // printf("%d child  %4d %4d %4d\n", i, getppid(), getpid(), fpid);
-            }
-            else{
-                 printf("%d parent %4d %4d\n", i,getpid(), fpid);
-            }
-        }
-    }
-    i = 10;
-    while(i-->0)
-    {
-         printf("%4d parent %4d\n", getppid(), getpid());
-         sleep(3);
-         if(ppid2 +1 == getpid()&&i<7)
-         {
-             int *ptr = NULL;
-             *ptr = 0;
-         }
-    }
-    return 0;
+	long int a = 1;
+	int x, y;
+	double b = 1.0;
+	clock_t start, end;
+	printf("请输入整数运算与浮点数运算概率（9  1）\n");
+	scanf_s("%d %d", &x, &y);            /*控制运行频率*/
+	start = clock();
+	for (int i = 0; i < int (x * 100000000); i++)                  /*整数操作*/
+	{
+		a *= 123456;
+	}
+	end = clock();
+	double  time1 = (double)(end - start) / CLK_TCK;
+	printf("整数运行时间为%f\n",time1 );
+	start = clock();
+	for (int i = 0; i < int( y * 100000000); i++)                /*浮点操作*/
+	{
+		b *= 1.23456;
+	}
+	end = clock();
+	double time2 = (double)(end - start) / CLK_TCK;
+	printf("浮点数运行时间%f\n", time2);
+	printf("加权运行时间为 %f\n", double((y*time2+x*time1)/(x+y)));
+	return 0;
 }
